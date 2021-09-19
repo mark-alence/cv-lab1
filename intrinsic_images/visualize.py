747ca81f04d3105452505969cc2ce1ff3fffd8fd
Mathieu <mathieucoenegracht@gmail.com>
2021-09-19 22:58:11 +0200

    Updated scripts

--- intrinsic_images/visualize.py
+++ intrinsic_images/visualize.py
@@ -3,20 +3,20 @@ import numpy as np
 import cv2
 
 
-def visualize(original_img, albedo_img, shadow_img, reconstructed_img):
+def visualize(original_img, albedo_img, shading_img, reconstructed_img):
     # Fill in this function. Remember to remove the pass command
     
     fig, axes = plt.subplots(2,2)
     axes[0,0].imshow(albedo_img.astype(np.uint8))
     axes[0,0].set_title('Albedo Image')
 
-    axes[0,1].imshow(shadow_img.astype(np.uint8))
+    axes[0,1].imshow(shading_img.astype(np.uint8))
     axes[0,1].set_title('Shading Image')
 
     axes[1,0].imshow(original_img.astype(np.uint8))
     axes[1,0].set_title('Original Image')
 
     axes[1,1].imshow(reconstructed_img.astype(np.uint8))
-    axes[1,1].set_title('Recolored Image')
+    axes[1,1].set_title('Reconstructed Image')
 
     plt.show()
